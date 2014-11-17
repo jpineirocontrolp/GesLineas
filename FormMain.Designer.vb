a69701e6d31975c04801d23749955442a5dbd3c7
Nacho <jpineiro@controlp.es>
2014-11-17 16:16:33 +0100

    2

--- FormMain.Designer.vb
+++ FormMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FormMain
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,17 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(1097, 538)
+        Me.IsMdiContainer = True
+        Me.Name = "FormMain"
+        Me.Text = "FormMain"
+        Me.ResumeLayout(False)
 
+    End Sub
 End Class
